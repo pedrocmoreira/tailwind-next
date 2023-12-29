abf8bff78bb6f8837a175b43a8407b29f1a818ed
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-12-29 14:42:35 -0300

    criando  input seguindo pattern de composição

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -4,16 +4,19 @@ import { NavItem } from "./NavItem";
 import { BarChart, CheckSquare, Cog, Flag, Home, LifeBuoy, Search, SquareStack, Users } from "lucide-react";
 import { UsedSpaceWidget } from "./UsedSpaceWidgetWidget";
 import { Profile } from "./Profile";
+import { InputControl, InputPrefix, InputRoot } from "../Input";
 
 export function Sidebar() {
   return (
     <aside className='flex flex-col gap-6 border-r border-zinc-200 px-5 py-8'>
       <Logo />
 
-      <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm -mx-1">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600" placeholder="Pesquisar" />
-      </div>
+      <InputRoot>
+        <InputPrefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </InputPrefix>
+        <InputControl/>
+      </InputRoot>
 
       <nav className="space-y-0.5">
         <NavItem
